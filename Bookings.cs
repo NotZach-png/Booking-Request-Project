03d9b74c8f51119786ad6b614381d62e3f535a0c
Ludwig <ludwigludick2004@gmail.com>
2026-05-09 19:49:37 +0200

    Re-uploaded project
    Project was re-uploaded for a few new class files that were created and fixing a few bugs that came with it.

---
+++ Bookings.cs
@@ -0,0 +1,66 @@
+﻿using Milestone2;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PRG261_Milestone1
+{
+    
+
+    public delegate void ApprovedBooking(string message);
+    public delegate void RejectedBooking(string message);
+    public delegate void ConditionalBooking(string message);
+    public class Bookings
+    {
+        // Getting our student class into our Booking class to access its values
+        readonly Student ExStudent = new Student();
+
+        public Student Student { get; set; }
+        public int YearOfStudy { get; set; }
+        public string EquipType { get; set; }
+        public int DurationInHours { get; set; }
+        public bool Training { get; set; }
+        public int ActiveBookings { get; set; }
+        public string Status { get; set; }
+
+        // Events
+        public event ApprovedBooking Approve;
+        public event RejectedBooking Reject;
+        public event ConditionalBooking Conditional;
+
+        // Setting up constructor
+        public Bookings(Student student, int year, string equip, int duration, bool train, int activeBookings)
+        {
+            // Calling formatted globalization object so that we don't have to stress to much about formatting values
+            TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+
+            this.Student = student;
+            this.YearOfStudy = year;
+            this.EquipType = textInfo.ToTitleCase(equip.ToLower());
+            this.DurationInHours = duration;
+            this.Training = train;
+            this.ActiveBookings = activeBookings;
+            this.Status = "Pending";
+        }
+
+        public void CheckStatus()
+        {
+            if (this.Status == "Approved")
+            {
+                Approve?.Invoke("Your request has been automatically APPROVED...");
+            }
+            else if (this.Status == "Conditional")
+            {
+                Conditional?.Invoke("Your request is under CONDTIONALLY APPROVED...");
+            }
+            else if (this.Status == "Rejected")
+            {
+                Reject?.Invoke("Your request has been REJECTED...");
+            }
+        }
+    }        
+}
---
+++ Student.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milestone2
+{
+    // This class is used just for the students information
+    public class Student
+    {
+        // Encapsulating these values
+        private string fname;
+        private string lname;
+        private string stunum;
+
+        readonly TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+        public string FName
+        {
+            get { return fname; }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || value.Length < 2 || !value.All(char.IsLetter))
+                    throw new ArgumentException("First name must be more than 2 letters and only contain letters not numbers.");
+                fname = textInfo.ToTitleCase(value.ToLower());
+            }
+        }
+        public string LName
+        {
+            get { return lname; }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || value.Length < 2 || !value.All(char.IsLetter))
+                    throw new ArgumentException("Your last name must be more than 2 letters and only contain letters not numbers.");
+                lname = textInfo.ToTitleCase(value.ToLower());
+            }
+        }
+        public string StuNum
+        {
+            get { return stunum; }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || value.Length != 6 || !value.All(char.IsDigit))
+                    throw new ArgumentException("Length of student number is 6 characters and must only consist of digits");
+                stunum = value;
+            }
+        }
+
+        public Student()
+        {
+        }
+
+        public Student(string fname, string lname, string stunum)
+        {
+            this.FName = fname;
+            this.LName = lname;
+            this.StuNum = stunum;
+        }
+
+    }
+}
